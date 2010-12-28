5bdb101006ea310fbc38202ef7108f656bfe032f
Raphael Speyer <rspeyer@gmail.com>
2010-12-29 08:29:30 +1100

    updated comments in signature

--- ospecl.mli
+++ ospecl.mli
@@ -1,12 +1,12 @@
-(* a description of a component's desired behavour *)
+(* an executable description of a component's desired behavour *)
 type spec
 (* information about how the component conforms to the spec *)
 type result = Pass of string | Fail of string | Error of string * exn
 
-(* logically group specs *)
-val describe : string -> spec list -> spec
 (* build a single spec from a description and test function *)
 val it : string -> (unit -> unit) -> spec
+(* logically group specs *)
+val describe : string -> spec list -> spec
 
 (* put expectations about values in your test functions *)
 val expect: 'a -> ('a -> bool) -> unit -> unit
